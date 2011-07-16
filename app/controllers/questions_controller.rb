bda1e044afa32b3621c35717537dd31496be7489
Geno Patterson <genosis@yahoo.com>
2011-07-16 11:26:22 -0500

    replacing email address with username

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   # GET /questions.xml
   def index
     @search = Question.search params[:search]
-    @questions = @search.paginate :page => params[:page], :per_page => 2
+    @questions = @search.paginate :page => params[:page], :per_page => 5
 
     respond_to do |format|
       format.html # index.html.erb
